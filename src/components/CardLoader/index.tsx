51c32ac224fdb0d702a8de07502112ee9717a0d5
Ricardo Campos <ricardo.campos@encora.com>
2022-09-19 10:37:40 -0300

    feat: loader box in separate component

--- src/components/CardLoader/index.tsx
+++ src/components/CardLoader/index.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Loader } from 'shared-components';
-
-const LoaderDiv = styled.div`
-  background: #003366;
-  padding: 20px;
-  margin: 0 auto;
-  border: 1px solid transparent;
-  border-radius: 0.25rem;
-  text-align: center;
-  color: #FFF;
-`;
+import LoaderBox from '../LoaderBox';
 
 function CardLoader() {
   return (
-    <LoaderDiv>
+    <LoaderBox>
       Please, wait!
 
       <Loader />
-    </LoaderDiv>
+    </LoaderBox>
   );
 }
 
