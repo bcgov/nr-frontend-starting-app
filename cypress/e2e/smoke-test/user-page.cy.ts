b8fbea79c0224c4e64095123cb19feb154080afa
Dogival Ferreira <dogival.silva@encora.com>
2022-10-19 16:40:53 -0300

    fix test cases / add new tests

--- cypress/e2e/smoke-test/user-page.cy.ts
+++ cypress/e2e/smoke-test/user-page.cy.ts
@@ -3,7 +3,13 @@ describe('user form test', () => {
     firstName: string,
     lastName: string
   };
-  let validMessage: string;
+  let msg: {
+    submit: string,
+    delete: string,
+    empty: string,
+    invalid: string,
+    duplicate: string
+  };
 
   beforeEach(() => {
     cy.visit('/form');
@@ -12,8 +18,8 @@ describe('user form test', () => {
     cy.fixture('user').then((user) => {
       testUser = user;
     });
-    cy.fixture('messages').then((msg) => {
-      validMessage = msg.validFeedback;
+    cy.fixture('messages').then((messages) => {
+      msg = messages;
     });
   });
 
@@ -22,15 +28,14 @@ describe('user form test', () => {
     cy.deleteUser(testUser.firstName, testUser.lastName);
 
     // Create the test user
-    cy.get('#first-name').type(testUser.firstName);
-    cy.getByDataTest('feedback-element').should('have.class', 'valid-feedback').and('have.text', validMessage);
-    cy.get('#last-name').type(testUser.lastName);
-    cy.getByDataTest('feedback-element').last().should('have.class', 'valid-feedback').and('have.text', validMessage);
-    cy.get('button').contains('Submit').click();
+    cy.getByDataTest('input-first').type(testUser.firstName).blur();
+    cy.getByDataTest('input-last').type(testUser.lastName).blur();
+    cy.getByDataTest('button-submit').contains('Submit').click();
 
     // Check that the user is created in the UI
     cy.intercept(`${Cypress.env('apiUrl')}/users/find-all`).as('getUsers');
     cy.wait('@getUsers');
+    cy.contains(msg.submit);
     cy.contains(testUser.firstName);
     cy.contains(testUser.lastName);
   });
@@ -40,11 +45,53 @@ describe('user form test', () => {
     cy.createUser(testUser.firstName, testUser.lastName);
     cy.reload();
 
-    // Delete user and verify that is deleted in the API
+    // Delete user and verify that is deleted in the UI
     cy.contains('td', testUser.firstName).parent('tr').within(() => {
-      cy.get('td').eq(2).contains('button', 'Delete me').click();
+      cy.get('td').eq(3).contains('button', 'Delete').click();
     });
-    cy.intercept('DELETE', `${Cypress.env('apiUrl')}/users/${testUser.firstName}/${testUser.lastName}`).as('delUser');
-    cy.wait('@delUser').its('response.statusCode').should('eq', 200);
+    cy.intercept(`${Cypress.env('apiUrl')}/users/find-all`).as('getUsers');
+    cy.wait('@getUsers');
+    cy.contains(msg.delete);
+    cy.contains(testUser.firstName).should('not.exist');
+    cy.contains(testUser.lastName).should('not.exist');
+  });
+
+  it('throws error with empty username', () => {
+    cy.getByDataTest('input-first').focus().blur();
+    cy.getByDataTest('input-last').focus().blur();
+    cy.getByDataTest('button-submit').contains('Submit').click();
+
+    cy.get('#input-first-error-msg').should('have.text', msg.empty);
+    cy.get('#input-last-error-msg').should('have.text', msg.empty);
+    cy.get('#error-banner').within(() => {
+      cy.get('.cds--inline-notification__subtitle').should('have.text', msg.invalid);
+    });
+  });
+
+  it('throws error at duplicate username', () => {
+    // Creating the user to duplicate after
+    cy.createUser(testUser.firstName, testUser.lastName);
+    cy.reload();
+
+    // Try to create a duplicate user
+    cy.getByDataTest('input-first').type(testUser.firstName).blur();
+    cy.getByDataTest('input-last').type(testUser.lastName).blur();
+    cy.getByDataTest('button-submit').contains('Submit').click();
+
+    // Check for the error message
+    cy.get('#error-banner').within(() => {
+      cy.get('.cds--inline-notification__subtitle').should('have.text', msg.duplicate);
+    });
+  });
+
+  it('empty fields after resetting form', () => {
+    // Fill the form inputs and reset
+    cy.getByDataTest('input-first').type(testUser.firstName).blur();
+    cy.getByDataTest('input-last').type(testUser.lastName).blur();
+    cy.getByDataTest('button-reset').contains('Reset').click();
+
+    // Check if all the inputs are empty
+    cy.getByDataTest('input-first').should('have.text', '');
+    cy.getByDataTest('input-last').should('have.text', '');
   });
 });
