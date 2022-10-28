70e53ab5dbfefb6e964f9d0c2a953794a0140da0
Ricardo Campos <ricardo.campos@encora.com>
2022-10-28 13:54:10 -0300

    feat: updated provier do be injected in a better place

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   BrowserRouter, Routes, Route
 } from 'react-router-dom';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
 
 import './custom.scss';
 
@@ -9,14 +10,24 @@ import Layout from './layout/PublicLayout';
 import Home from './views/Home';
 import Form from './views/Form';
 import Table from './views/Table';
+import keycloak from './keycloak';
+
+const initOptions = {
+  onLoad: 'check-sso',
+  silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
+  pkceMethod: 'S256'
+};
 
 /**
  * Create an app structure conaining all the routes.
  *
  * @returns {JSX.Element} instance of the app ready to use.
  */
-function App() {
-  return (
+const App: React.FC = () => (
+  <ReactKeycloakProvider
+    initOptions={initOptions}
+    authClient={keycloak}
+  >
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
@@ -26,7 +37,7 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-  );
-}
+  </ReactKeycloakProvider>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,52 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { ReactKeycloakProvider } from '@react-keycloak/web';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import keycloak from './keycloak';
-
-const initOptions = {
-  onLoad: 'check-sso',
-  silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
-  pkceMethod: 'S256'
-};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-/**
- * If you need to debug:
- * - add onEvent={eventLogger} and onTokens={tokenLogger} to the ReactKeycloakProvider
- *
- * const eventLogger = (event: unknown, error: unknown) => {
- *   console.log('onKeycloakEvent', event, error);
- * };
- * const tokenLogger = (tokens: unknown) => {
- *   console.log('onKeycloakTokens', tokens);
- * };
- */
-
 root.render(
   <React.StrictMode>
-    <ReactKeycloakProvider
-      authClient={keycloak}
-      initOptions={initOptions}
-    >
-      <App />
-    </ReactKeycloakProvider>
+    <App />
   </React.StrictMode>
 );
-
-/*
-UserService.initKeycloak()
-  .then((authenticated: boolean) => {
-    console.log(`KeyCloak started! User is autenticated: ${authenticated}! ${new Date()}`);
-  }).catch((err) => {
-    console.log('Some error ocurred:', err);
-  }).finally(() => {});
-*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
