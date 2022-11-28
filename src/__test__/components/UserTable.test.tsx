566f6a1edb58525fef9d831f894fb8d369c7d9e6
Ricardo Campos <ricardo.campos@encora.com>
2022-11-28 09:47:50 -0300

    test: userTable component test passing

--- src/__test__/components/UserTable.test.tsx
+++ src/__test__/components/UserTable.test.tsx
@@ -5,6 +5,7 @@ import { render } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import UserTable from '../../components/UserTable';
 import SampleUser from '../../types/SampleUser';
+import { AuthProvider } from '../../contexts/AuthContext';
 
 describe('the UserTable component', () => {
   const users: SampleUser[] = [{
@@ -17,22 +18,24 @@ describe('the UserTable component', () => {
   const tableHeaders: string[] = ['Column1', 'Column2', 'Column3'];
 
   it('should have the correct headers', () => {
-    /*
     const { getByTestId } = render(
-      <UserTable elements={users} deleteFn={() => {}} headers={tableHeaders} />
+      <AuthProvider>
+        <UserTable elements={users} deleteFn={() => {}} headers={tableHeaders} />
+      </AuthProvider>
     );
     tableHeaders.forEach((element, i) => {
       expect(getByTestId(`header-${element}-${i}`).textContent).toBe(element);
     });
-    */
   });
 
   it('should match the snapshot', () => {
-    /*
     const tree = renderer
-      .create(<UserTable elements={users} deleteFn={() => {}} headers={tableHeaders} />)
+      .create(
+        <AuthProvider>
+          <UserTable elements={users} deleteFn={() => {}} headers={tableHeaders} />
+        </AuthProvider>
+      )
       .toJSON();
     expect(tree).toMatchSnapshot();
-    */
   });
 });
