594d2a2fb0363da398e238e727e85290a8806fbd
Wennio Oliveira <wennio.oliveira@encora.com>
2022-12-26 16:46:22 -0400

    test: skip user page test cases

--- cypress/e2e/smoke-test/user-page.cy.ts
+++ cypress/e2e/smoke-test/user-page.cy.ts
@@ -15,7 +15,7 @@ describe('user form test', () => {
     });
   });
 
-  it('create a valid user', () => {
+  it.skip('create a valid user', () => {
     // Removing the user first to avoid false negatives
     cy.deleteUser(testUser.firstName, testUser.lastName);
 
@@ -32,7 +32,7 @@ describe('user form test', () => {
     cy.contains(testUser.lastName);
   });
 
-  it('delete a user', () => {
+  it.skip('delete a user', () => {
     // Creating the user first to avoid false negatives
     cy.createUser(testUser.firstName, testUser.lastName);
     cy.reload();
@@ -48,7 +48,7 @@ describe('user form test', () => {
     cy.contains(testUser.lastName).should('not.exist');
   });
 
-  it('empty fields after resetting form', () => {
+  it.skip('empty fields after resetting form', () => {
     // Fill the form inputs and reset
     cy.getByDataTest('input-first').type(testUser.firstName).blur();
     cy.getByDataTest('input-last').type(testUser.lastName).blur();
@@ -59,7 +59,7 @@ describe('user form test', () => {
     cy.getByDataTest('input-last').should('have.text', '');
   });
 
-  it('error with empty username', () => {
+  it.skip('error with empty username', () => {
     cy.getByDataTest('input-first').focus().blur();
     cy.getByDataTest('input-last').focus().blur();
     cy.getByDataTest('button-submit').contains('Submit').click();
@@ -71,7 +71,7 @@ describe('user form test', () => {
     });
   });
 
-  it('error at duplicate username', () => {
+  it.skip('error at duplicate username', () => {
     // Creating the user to duplicate after
     cy.createUser(testUser.firstName, testUser.lastName);
     cy.reload();
@@ -87,7 +87,7 @@ describe('user form test', () => {
     });
   });
 
-  it('error typing less than 2 characters', () => {
+  it.skip('error typing less than 2 characters', () => {
     // Fill the form inputs with invalid data
     cy.getByDataTest('input-first').type('a').blur();
     cy.get('#input-first-error-msg').should('have.text', msg.input.charNumberFirst);
